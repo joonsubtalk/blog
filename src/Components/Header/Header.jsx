54716de6c4af7217bd92f6183871a61d293ddb33
JoonSub Chung <jchung2@mediatemple.net>
2017-10-25 08:26:28 -0700

    using bootstraps grid system

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,12 +4,12 @@ import './c-Header.css';
 const Header = (props) => (
 	<header className="o-header">
 		<div className="o-header__content">
-			<div className="grid container">
+			<div className="container">
 				<div className="row">
-					<div className="col-md-6">
+					<div className="col-sm-6">
 						Joonsub.com
 					</div>
-					<div className="col-md-6">
+					<div className="col-sm-6">
 						Who What When
 					</div>
 				</div>
